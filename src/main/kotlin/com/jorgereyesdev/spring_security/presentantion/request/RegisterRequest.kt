cc5b688e0a0fe98d73370126b4c715c65a528f8a
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2025-01-18 23:39:59 -0500

    add RegisterRequest.kt

---
+++ src/main/kotlin/com/jorgereyesdev/spring_security/presentantion/request/RegisterRequest.kt
@@ -0,0 +1,21 @@
+package com.jorgereyesdev.spring_security.presentantion.request
+
+import com.jorgereyesdev.spring_security.domain.models.User
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+
+data class RegisterRequest(
+    @NotBlank
+    @Size(min = 3, max = 25)
+    val username: String,
+
+    @NotBlank
+    @Size(min = 6, max = 50)
+    val password: String,
+)
+
+fun RegisterRequest.toDomain() = User(
+    username = this.username,
+    password = this.password,
+    enable = true,
+)
