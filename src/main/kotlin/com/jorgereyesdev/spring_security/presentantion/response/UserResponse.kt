1a729b289b94f7859d44ab28e7f93a62a84de0c7
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2025-01-18 23:39:31 -0500

    change UserResponseDTO.kt to UserResponse.kt

---
+++ src/main/kotlin/com/jorgereyesdev/spring_security/presentantion/response/UserResponse.kt
@@ -0,0 +1,10 @@
+package com.jorgereyesdev.spring_security.presentantion.response
+
+import com.fasterxml.jackson.annotation.JsonInclude
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class UserResponse(
+    val id: Int?,
+    val username: String,
+    val enabled: Boolean,
+)
