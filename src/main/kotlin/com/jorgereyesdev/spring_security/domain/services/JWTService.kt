270d98128516905ca5d0f637b5f7d6a77eae7809
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2025-01-18 23:36:00 -0500

    add JWTService.kt

---
+++ src/main/kotlin/com/jorgereyesdev/spring_security/domain/services/JWTService.kt
@@ -0,0 +1,10 @@
+package com.jorgereyesdev.spring_security.domain.services
+
+import com.jorgereyesdev.spring_security.domain.models.User
+
+interface JWTService {
+    fun generateToken(user: User): String
+    fun generateRefreshToken(user: User): String
+    fun isTokenValid(token: String, username: String): Boolean
+    fun getUsernameFromToken(token: String): String?
+}
