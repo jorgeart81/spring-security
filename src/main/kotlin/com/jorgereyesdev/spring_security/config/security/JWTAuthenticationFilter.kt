d8dad3578951163b102d8aa8277efd5381e52525
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2025-01-20 22:18:51 -0500

    add JwtAuthenticationFilter

---
+++ src/main/kotlin/com/jorgereyesdev/spring_security/config/security/JWTAuthenticationFilter.kt
@@ -0,0 +1,82 @@
+package com.jorgereyesdev.spring_security.config.security
+
+import com.jorgereyesdev.spring_security.config.Constants.*
+import com.jorgereyesdev.spring_security.domain.services.JWTService
+import com.jorgereyesdev.spring_security.infrastructure.repositories.UserRepository
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.HttpHeaders
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
+import org.springframework.stereotype.Component
+import org.springframework.util.StringUtils
+import org.springframework.web.filter.OncePerRequestFilter
+
+@Component
+class JwtAuthenticationFilter(
+    val userRepository: UserRepository,
+    val jwtService: JWTService,
+    val userDetailsService: UserDetailsService
+) :
+    OncePerRequestFilter() {
+
+    override fun doFilterInternal(
+        request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain
+    ) {
+        if (request.servletPath.contains("/auth")) {
+            filterChain.doFilter(request, response)
+            return
+        }
+
+        val token = getTokenFromRequest(request)
+        if (token == null) {
+            filterChain.doFilter(request, response)
+            return
+        }
+
+        val username = jwtService.getUsernameFromToken(token)
+
+        if (username.isNullOrEmpty()) {
+            filterChain.doFilter(request, response)
+            return
+        }
+
+        val userDetails = userDetailsService.loadUserByUsername(username)
+        val userEntity = userRepository.findByUsername(userDetails.username)
+
+        if (userEntity == null) {
+            filterChain.doFilter(request, response)
+            return
+        }
+
+        val isTokenValid = jwtService.isTokenValid(token, userEntity.username)
+
+        if (!isTokenValid) return
+
+        configureAuthentication(userDetails, request)
+
+        filterChain.doFilter(request, response)
+    }
+
+    private fun getTokenFromRequest(request: HttpServletRequest): String? {
+        val authHeader = request.getHeader(HttpHeaders.AUTHORIZATION)
+
+        return if (StringUtils.hasText(authHeader) && authHeader.startsWith(Authorization.BEARER)) {
+            authHeader.substring(7)
+        } else {
+            null
+        }
+    }
+
+    private fun configureAuthentication(userDetails: UserDetails, request: HttpServletRequest) {
+        val authenticationToken =
+            UsernamePasswordAuthenticationToken(userDetails, null, userDetails.authorities).apply {
+                details = WebAuthenticationDetailsSource().buildDetails(request)
+            }
+        SecurityContextHolder.getContext().authentication = authenticationToken
+    }
+}
