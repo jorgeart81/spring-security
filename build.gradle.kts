25c0d78884187833f2a93e65b47c429c3c1f2688
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2025-01-20 22:21:20 -0500

    add kotlinx-coroutines-reactor and reactive-streams dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	implementation("org.reactivestreams:reactive-streams:1.0.4")
 	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
