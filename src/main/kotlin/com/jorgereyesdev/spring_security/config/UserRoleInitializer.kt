d9b0f84f89c5a88487b381ae83798d56bbdcf7e9
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2025-02-03 22:37:49 -0500

    UserRoleInitializer.kt

--- src/main/kotlin/com/jorgereyesdev/spring_security/config/UserRoleInitializer.kt
+++ src/main/kotlin/com/jorgereyesdev/spring_security/config/UserRoleInitializer.kt
@@ -5,15 +5,22 @@ import com.jorgereyesdev.spring_security.domain.models.RoleName
 import com.jorgereyesdev.spring_security.domain.models.isAllowedTo
 import com.jorgereyesdev.spring_security.infrastructure.entities.PermissionEntity
 import com.jorgereyesdev.spring_security.infrastructure.entities.RoleEntity
+import com.jorgereyesdev.spring_security.infrastructure.entities.UserEntity
 import com.jorgereyesdev.spring_security.infrastructure.repositories.PermissionRepository
 import com.jorgereyesdev.spring_security.infrastructure.repositories.RoleRepository
+import com.jorgereyesdev.spring_security.infrastructure.repositories.UserRepository
 import org.springframework.boot.CommandLineRunner
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 
 @Component
-class RoleInitializer(val roleRepository: RoleRepository, val permissionRepository: PermissionRepository) :
-    CommandLineRunner {
+class UserRoleInitializer(
+    val roleRepository: RoleRepository,
+    val permissionRepository: PermissionRepository,
+    val userRepository: UserRepository,
+    val passwordEncoder: PasswordEncoder,
+) : CommandLineRunner {
 
     private val defaultRoles = listOf(RoleName.ADMIN, RoleName.USER)
     private val defaultPermissions =
@@ -37,12 +44,33 @@ class RoleInitializer(val roleRepository: RoleRepository, val permissionReposito
 
         val roles = defaultRoles.map { roleName ->
             RoleEntity(
-                name = roleName,
-                permissions = permissionsByRoleName(roleName)
+                name = roleName, permissions = permissionsByRoleName(roleName)
             )
         }
-        roleRepository.saveAll(roles)
 
+        val rolesEntities = roleRepository.saveAll(roles)
+        rolesEntities.asSequence().forEach {
+            if (it.name == RoleName.ADMIN) {
+                createFirstAdmin(it)
+            }
+        }
+    }
 
+    private fun createFirstAdmin(role: RoleEntity) {
+        val user = UserEntity(
+            username = "admin",
+            password = "",
+            securityStamp = "",
+            accountNonExpired = true,
+            accountNonLocked = true,
+            credentialsNonExpired = true,
+            enabled = true,
+            roles = mutableListOf(role)
+        )
+
+        user.setSecurePassword("Admin1234", passwordEncoder)
+            .generateSecurityStamp()
+
+        userRepository.save(user)
     }
 }
