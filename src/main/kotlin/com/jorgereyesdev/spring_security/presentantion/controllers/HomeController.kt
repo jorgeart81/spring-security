7def68244a4691a4ac8ebba3b2f28debeec5ba03
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2025-02-03 22:42:12 -0500

    exclude redirectToSwagger from api-doc

--- src/main/kotlin/com/jorgereyesdev/spring_security/presentantion/controllers/HomeController.kt
+++ src/main/kotlin/com/jorgereyesdev/spring_security/presentantion/controllers/HomeController.kt
@@ -1,5 +1,6 @@
 package com.jorgereyesdev.spring_security.presentantion.controllers
 
+import io.swagger.v3.oas.annotations.Hidden
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.view.RedirectView
@@ -7,6 +8,7 @@ import org.springframework.web.servlet.view.RedirectView
 @RestController
 class HomeController {
 
+    @Hidden
     @GetMapping("/swagger-ui")
     fun redirectToSwagger(): RedirectView {
         return RedirectView("/swagger-ui/index.html")
